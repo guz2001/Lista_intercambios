48c0b5ffcae2608d2b39b83807f777dcd2c303d5
guz2001 <guz20dgc@gmail.com>
2026-03-03 23:34:20 -0500

    creacion de getter y setter

--- src/main/java/com/udea/nutricion/ComposicionNutricional.java
+++ src/main/java/com/udea/nutricion/ComposicionNutricional.java
@@ -1,6 +1,256 @@
 package com.udea.nutricion;
 
 public class ComposicionNutricional {
-    private double calorias;
-    private  double proteinas;
+    //macronutrientes y energia
+    private double kcal;
+    private double proteinaG;
+    private double grasaTotalG;      // GT. g.
+    private double grasasSaturadasG;  // AGS. g.
+    private double grasasMonoG;       // AGM. g.
+    private double grasasPoliG;       // AGP. g.
+    private double colesterolMg;      // Col. mg.
+    private double carbohidratosG;    // CHO. g.
+    private double fibraDieteticaG;   // FD. g.
+
+    // Minerales 🧂
+    private double calcioMg;          // Ca mg.
+    private double fosforoMg;         // P mg.
+    private double hierroMg;          // Fe mg.
+    private double sodioMg;           // Na mg.
+    private double potasioMg;         // K mg.
+    private double magnesioMg;        // Mg mg.
+    private double zincMg;            // Zn mg.
+    private double cobreMg;           // Cu mg.
+    private double manganesoMg;       // Mn mg.
+
+    // Vitaminas 🍊
+    private double vitaminaAER;       // Vit A ER.
+    private double tiaminaMg;         // Tiam. mg.
+    private double riboflavinaMg;     // Ribof. mg.
+    private double niacinaMg;         // Niac. mg.
+    private double acidoPantotenicoMg; // Ac. Pant. mg.
+    private double piridoxinaMg;      // Piridox. mg.
+    private double folatoMcg;         // Fol. mcg. EFD
+    private double vitaminaB12Mcg;    // Vit. B12 mcg.
+    private double vitaminaCMg;       // Vit. C. mg.
+
+    public double getGrasasPoliG() {
+        return grasasPoliG;
+    }
+
+    public void setGrasasPoliG(double grasasPoliG) {
+        this.grasasPoliG = grasasPoliG;
+    }
+
+    public double getKcal() {
+        return kcal;
+    }
+
+    public void setKcal(double kcal) {
+        this.kcal = kcal;
+    }
+
+    public double getProteinaG() {
+        return proteinaG;
+    }
+
+    public void setProteinaG(double proteinaG) {
+        this.proteinaG = proteinaG;
+    }
+
+    public double getGrasaTotalG() {
+        return grasaTotalG;
+    }
+
+    public void setGrasaTotalG(double grasaTotalG) {
+        this.grasaTotalG = grasaTotalG;
+    }
+
+    public double getGrasasSaturadasG() {
+        return grasasSaturadasG;
+    }
+
+    public void setGrasasSaturadasG(double grasasSaturadasG) {
+        this.grasasSaturadasG = grasasSaturadasG;
+    }
+
+    public double getGrasasMonoG() {
+        return grasasMonoG;
+    }
+
+    public void setGrasasMonoG(double grasasMonoG) {
+        this.grasasMonoG = grasasMonoG;
+    }
+
+    public double getColesterolMg() {
+        return colesterolMg;
+    }
+
+    public void setColesterolMg(double colesterolMg) {
+        this.colesterolMg = colesterolMg;
+    }
+
+    public double getCarbohidratosG() {
+        return carbohidratosG;
+    }
+
+    public void setCarbohidratosG(double carbohidratosG) {
+        this.carbohidratosG = carbohidratosG;
+    }
+
+    public double getFibraDieteticaG() {
+        return fibraDieteticaG;
+    }
+
+    public void setFibraDieteticaG(double fibraDieteticaG) {
+        this.fibraDieteticaG = fibraDieteticaG;
+    }
+
+    public double getCalcioMg() {
+        return calcioMg;
+    }
+
+    public void setCalcioMg(double calcioMg) {
+        this.calcioMg = calcioMg;
+    }
+
+    public double getFosforoMg() {
+        return fosforoMg;
+    }
+
+    public void setFosforoMg(double fosforoMg) {
+        this.fosforoMg = fosforoMg;
+    }
+
+    public double getHierroMg() {
+        return hierroMg;
+    }
+
+    public void setHierroMg(double hierroMg) {
+        this.hierroMg = hierroMg;
+    }
+
+    public double getSodioMg() {
+        return sodioMg;
+    }
+
+    public void setSodioMg(double sodioMg) {
+        this.sodioMg = sodioMg;
+    }
+
+    public double getPotasioMg() {
+        return potasioMg;
+    }
+
+    public void setPotasioMg(double potasioMg) {
+        this.potasioMg = potasioMg;
+    }
+
+    public double getMagnesioMg() {
+        return magnesioMg;
+    }
+
+    public void setMagnesioMg(double magnesioMg) {
+        this.magnesioMg = magnesioMg;
+    }
+
+    public double getZincMg() {
+        return zincMg;
+    }
+
+    public void setZincMg(double zincMg) {
+        this.zincMg = zincMg;
+    }
+
+    public double getCobreMg() {
+        return cobreMg;
+    }
+
+    public void setCobreMg(double cobreMg) {
+        this.cobreMg = cobreMg;
+    }
+
+    public double getManganesoMg() {
+        return manganesoMg;
+    }
+
+    public void setManganesoMg(double manganesoMg) {
+        this.manganesoMg = manganesoMg;
+    }
+
+    public double getVitaminaAER() {
+        return vitaminaAER;
+    }
+
+    public void setVitaminaAER(double vitaminaAER) {
+        this.vitaminaAER = vitaminaAER;
+    }
+
+    public double getTiaminaMg() {
+        return tiaminaMg;
+    }
+
+    public void setTiaminaMg(double tiaminaMg) {
+        this.tiaminaMg = tiaminaMg;
+    }
+
+    public double getRiboflavinaMg() {
+        return riboflavinaMg;
+    }
+
+    public void setRiboflavinaMg(double riboflavinaMg) {
+        this.riboflavinaMg = riboflavinaMg;
+    }
+
+    public double getNiacinaMg() {
+        return niacinaMg;
+    }
+
+    public void setNiacinaMg(double niacinaMg) {
+        this.niacinaMg = niacinaMg;
+    }
+
+    public double getAcidoPantotenicoMg() {
+        return acidoPantotenicoMg;
+    }
+
+    public void setAcidoPantotenicoMg(double acidoPantotenicoMg) {
+        this.acidoPantotenicoMg = acidoPantotenicoMg;
+    }
+
+    public double getPiridoxinaMg() {
+        return piridoxinaMg;
+    }
+
+    public void setPiridoxinaMg(double piridoxinaMg) {
+        this.piridoxinaMg = piridoxinaMg;
+    }
+
+    public double getFolatoMcg() {
+        return folatoMcg;
+    }
+
+    public void setFolatoMcg(double folatoMcg) {
+        this.folatoMcg = folatoMcg;
+    }
+
+    public double getVitaminaB12Mcg() {
+        return vitaminaB12Mcg;
+    }
+
+    public void setVitaminaB12Mcg(double vitaminaB12Mcg) {
+        this.vitaminaB12Mcg = vitaminaB12Mcg;
+    }
+
+    public double getVitaminaCMg() {
+        return vitaminaCMg;
+    }
+
+    public void setVitaminaCMg(double vitaminaCMg) {
+        this.vitaminaCMg = vitaminaCMg;
+    }
+
+    public ComposicionNutricional{
+        TODO:
+    }
 }
